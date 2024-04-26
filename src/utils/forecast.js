a2d50e56c34b40923b6ff80d6ea397e8dfcee3e8
theMalmstadt <jacob_malmstadt@hotmail.com>
2024-04-26 15:07:39 -0700

    Fixed punctuation in forecast readout.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location.')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The current time is ' + body.current.observation_time)
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The current time is ' + body.current.observation_time + ".")
         }
     })
 }
