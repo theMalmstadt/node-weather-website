bd5e803f77bd51a2be73c35ed844baaed01eebbb
theMalmstadt <jacob_malmstadt@hotmail.com>
2024-04-26 15:13:45 -0700

    Fixed wording on forecast readout.

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,13 +3,12 @@ const request = require('request')
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=ca9c989fed565ebe7d1edf6531f90b69&query='+ latitude +','+ longitude +'&units=f'
     request({ url, json: true }, (error, {body}) => {
-        console.log(body)
         if (error) {
             callback('Unable to connect to weather service!')
         } else if (body.error) {
             callback('Unable to find location.')
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The current time is ' + body.current.observation_time + ".")
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The time this forecast was take was at ' + body.current.observation_time + ".")
         }
     })
 }
